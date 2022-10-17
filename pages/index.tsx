13908c4e1d994e17a6ce41a1e9cf31118fb9c492
headbwoi <tochukwujohn24@gmail.com>
2022-10-17 02:59:53 +0100

    added custom document

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,12 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Create Next App</title>
-        <meta http-equiv="X-UA-Compatible" content="IE=7" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta http-equihttpEquiv="X-UA-Compatible" content="IE=7" />
+        <meta http-equihttpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta
+          http-equihttpEquiv="Content-Type"
+          content="text/html;charset=UTF-8"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
@@ -19,9 +22,14 @@ const Home: NextPage = () => {
           content="React, Nextjs, Portfolio, Headbwoi, Tochukwu, Full stack"
         />
         <link rel="icon" href="/favicon.ico" />
+      
       </Head>
 
-      <main></main>
+      <header className="">hey im a header</header>
+
+      <main className="relative">
+        <div className="text-3xl text-red-500">heyy</div>
+      </main>
 
       <footer></footer>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      fontSize:{
+        large: "64px"
+      },
+    },
   },
   plugins: [],
 }
