c0daff0699f1fd28dc15ee49d0ef7555a4dd29ee
headbwoi <tochukwujohn24@gmail.com>
2022-10-17 10:18:12 +0100

    finished desktop navigation

--- components/Navigation/Desktop.tsx
+++ components/Navigation/Desktop.tsx
@@ -3,7 +3,7 @@ import LinkWrapper from "./LinkWrapper"
 const Desktop = () => {
   const linkArray = ["home", "about me", "portfolio", "experience", "contact"]
   return (
-    <div className="flex items-center space-x-8 h-[5.375rem] min-w-[33.0625rem] ">
+    <div className="flex items-center space-x-8 h-[5.375rem] min-w-[33.0625rem]">
       {linkArray.map((item, index) => (
         <LinkWrapper key={index} name={item} />
       ))}
