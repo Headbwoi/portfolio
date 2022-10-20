702b1dcf38eddcfd52c01c622b81ca04435a706f
headbwoi <tochukwujohn24@gmail.com>
2022-10-20 01:34:39 +0100

    edited hero text

--- components/Main/HeroSection/HeroSection.tsx
+++ components/Main/HeroSection/HeroSection.tsx
@@ -1,6 +1,6 @@
 const HeroSection = () => {
   return (
-    <div className="w-full pt-10">
+    <div className="w-full pt-14 lg:mt-0">
       <p className=" text-[6rem] lg:text-[12rem] font-bold text-gray select-none">
         HELLO
       </p>
@@ -9,7 +9,7 @@ const HeroSection = () => {
         {/* text */}
         <div className="relative w-full pb-8 lg:pb-0 lg:w-[48.375rem] mx-auto">
           {/* name */}
-          <div className=" font-bold text-white text-head-sm lg:text-head-lg font-pop flex justify-center space-x-8 pb-7 mb-10 md:mb-14 bg-body/14 text-center">
+          <div className=" font-bold text-white text-head-sm md:text-head-lg font-pop flex justify-center space-x-8 pb-7 mb-10 md:mb-14 bg-body/14 text-center">
             <p className="font-pop">{"👋I'm"}</p>
             <p className="font-pop text-cyan relative w-fit before:absolute before:-bottom-6 before:left-0 before:w-full before:h-1 before:block before:bg-cyan before:rounded-full ">
               John
@@ -17,9 +17,11 @@ const HeroSection = () => {
           </div>
           {/* brief about */}
           <div className="w-full text-center">
-            <p className="text-white text-lg md:text-head-sm lg:leading-[3rem]">
+            <p className="text-white text-lg md:text-head-sm md:leading-[4rem]">
               A Full Stack Engineer ,{" "}
-              <span className="text-cyan">MERN / T3 Stack Developer</span> ,
+              <span className="text-cyan block">
+                MERN / T3 Stack Developer,
+              </span>{" "}
               UI/UX Designer
             </p>
           </div>
