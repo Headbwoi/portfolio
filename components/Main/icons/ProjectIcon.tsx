3dc53d4b1f8d29a57b6e760a0d7b3f839adb0771
headbwoi <tochukwujohn24@gmail.com>
2022-10-21 02:10:09 +0100

    moved icons to main component

---
+++ components/Main/icons/ProjectIcon.tsx
@@ -0,0 +1,26 @@
+const ProjectIcon = () => {
+  return (
+    // <svg
+    //   xmlns="http://www.w3.org/2000/svg"
+    //   viewBox="0 0 576 512"
+    //   className="text-text group-hover:text-cyan w-6 h-6"
+    // >
+    //   <path
+    //     d="M0 80C0 53.5 21.5 32 48 32h96c26.5 0 48 21.5 48 48V96H384V80c0-26.5 21.5-48 48-48h96c26.5 0 48 21.5 48 48v96c0 26.5-21.5 48-48 48H432c-26.5 0-48-21.5-48-48V160H192v16c0 1.7-.1 3.4-.3 5L272 288h96c26.5 0 48 21.5 48 48v96c0 26.5-21.5 48-48 48H272c-26.5 0-48-21.5-48-48V336c0-1.7 .1-3.4 .3-5L144 224H48c-26.5 0-48-21.5-48-48V80z"
+    //     fill="currentColor"
+    //   />
+    // </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      className="text-text group-hover:text-cyan w-6 h-6"
+    >
+      <path
+        d="M184 48H328c4.4 0 8 3.6 8 8V96H176V56c0-4.4 3.6-8 8-8zm-56 8V96H64C28.7 96 0 124.7 0 160v96H192 320 512V160c0-35.3-28.7-64-64-64H384V56c0-30.9-25.1-56-56-56H184c-30.9 0-56 25.1-56 56zM512 288H320v32c0 17.7-14.3 32-32 32H224c-17.7 0-32-14.3-32-32V288H0V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V288z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
+
+export default ProjectIcon
