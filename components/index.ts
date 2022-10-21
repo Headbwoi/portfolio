9c9a5a148809a13d7475d001fc8e7c8e40ff3623
headbwoi <tochukwujohn24@gmail.com>
2022-10-21 02:25:09 +0100

    added dynamic Head sections

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
+export { default as HeadSection } from "./Head/HeadSection"
 export { default as Header } from "./Header/Header"
 export { default as HeroSection } from "./Main/HeroSection/HeroSection"
 export { default as HeroNav } from "./Main/HeroSection/HeroNav"
