4e96d51a2ca322bd6a2f82982afffc3764ea618d
headbwoi <tochukwujohn24@gmail.com>
2022-10-26 04:01:44 +0100

    made text responsive for md and lg breakpoints

--- components/Main/HeroSection/HeroSection.tsx
+++ components/Main/HeroSection/HeroSection.tsx
@@ -6,13 +6,15 @@ const HeroSection = () => {
     <div className="w-full pt-[20%] sm:pt-[25%] lg:pt-0 flex items-center justify-start sm:justify-center lg:justify-start flex-col min-h-screen">
       <div className="flex items-center justify-center flex-col w-full">
         {/* text */}
-        <div className="relative w-full pb-8 lg:pb-40 lg:w-[48.375rem] mx-auto">
+        <div className="relative w-full pb-8 lg:pb-40 mx-auto">
           {/* name */}
           <div className=" font-bold text-cyan font-pop  w-full">
-            <p className="font-pop text-xl mb-4 md:mb-6">Hi there, {"👋I'm"}</p>
-            <div className="flex items-center md:justify-center space-x-5 pb-7 font-pop text-text text-3xl sm:text-4xl md:text-6xl">
+            <p className="font-pop text-lg md:text-xl mb-4 md:mb-6">
+              Hi there, {"👋I'm"}
+            </p>
+            <div className="flex items-center space-x-5 pb-7 font-pop text-text text-3xl sm:text-4xl md:text-7xl">
               <p className=" w-fit">Tochukwu</p>
-              <p className="relative text-cyan before:absolute before:-bottom-2 before:left-0 before:w-full before:h-1 before:block before:bg-cyan before:rounded-full">
+              <p className="relative text-cyan before:absolute before:-bottom-2 before:left-0 before:w-full before:h-1 md:before:h-2 md:before:-bottom-3 before:block before:bg-cyan before:rounded-full">
                 John.
               </p>
             </div>
