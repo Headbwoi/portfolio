661367f4597e76b90f1baae8d66f094924f43606
headbwoi <tochukwujohn24@gmail.com>
2022-10-27 05:21:03 +0100

    increased accessibility

--- components/Main/HeroSection/HeroSection.tsx
+++ components/Main/HeroSection/HeroSection.tsx
@@ -14,12 +14,12 @@ const HeroSection = () => {
             <p className="font-pop text-lg md:text-xl mb-4 md:mb-6">
               Hi there, {"👋I'm"}
             </p>
-            <div className="flex items-center space-x-5 pb-7 font-pop text-text text-3xl sm:text-4xl md:text-[5rem] md:leading-[1.3]">
-              <p className=" w-fit">Tochukwu</p>
-              <p className="relative text-cyan before:absolute before:-bottom-2 before:left-0 before:w-full before:h-1 md:before:h-2 md:before:-bottom-3 before:block before:bg-cyan before:rounded-full">
+            <h1 className="flex items-center space-x-5 pb-7 font-pop text-text text-3xl sm:text-4xl md:text-[5rem] md:leading-[1.3]">
+              <span className=" w-fit">Tochukwu</span>
+              <span className="relative text-cyan before:absolute before:-bottom-2 before:left-0 before:w-full before:h-1 md:before:h-2 md:before:-bottom-3 before:block before:bg-cyan before:rounded-full">
                 John.
-              </p>
-            </div>
+              </span>
+            </h1>
           </div>
           {/* brief about */}
           <div className="w-full mb-6 ">
@@ -29,7 +29,7 @@ const HeroSection = () => {
           </div>
           {/* brief about info */}
           <div className="max-w-[33.75rem]">
-            <p className="text-text_Light text-lg md:text-xl md:leading-[1.3] ">
+            <p className="text-text_Light text-base md:text-xl md:leading-[1.3] ">
               {"i'm"} a <span className="text-cyan">software engineer</span>, a
               <span className="text-cyan"> full stack engineer</span> and
               occasionally i design exceptional user experiences. Currently
