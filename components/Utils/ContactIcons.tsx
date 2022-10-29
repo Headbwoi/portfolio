66c963aaa45f36eeb3a6ada8d403f2042c5d069c
headbwoi <tochukwujohn24@gmail.com>
2022-10-29 06:53:16 +0100

    hide contact icons on mobile

--- components/Utils/ContactIcons.tsx
+++ components/Utils/ContactIcons.tsx
@@ -3,10 +3,11 @@ import {
   faLinkedin,
   faTwitter,
 } from "@fortawesome/free-brands-svg-icons"
+import Link from "next/link"
 import IconLinks from "./IconLinks"
 const ContactIcons = () => {
   return (
-    <>
+    <div className="hidden md:block">
       {/* left */}
       <div className="flex items-center justify-center px-6 py-3 lg:py-0 lg:px-0 lg:w-10 border-cyan left-0 fixed z-30 bottom-0 lg:left-3 xl:left-10 right-auto">
         <ul className="flex lg:flex-col space-x-6 lg:space-x-0 lg:space-y-6 list-none  after:hidden lg:after:block after:h-24 after:w-[2px] after:mx-auto after:bg-white after:mt-4  ">
@@ -22,16 +23,24 @@ const ContactIcons = () => {
           <li className="w-5 h-5">
             <IconLinks
               iconName={faTwitter}
-              link="https://github.com/Headbwoi"
+              link="https://twitter.com/Headbwoi_1"
             />
           </li>
         </ul>
       </div>
       {/* right */}
-      <div className="flex flex-col items-center justify-center w-10 border-cyan fixed z-30 bottom-0 lg:right-3 right-10 left-auto">
-        <ul className="list-none after:block after:h-24 after:w-[2px] after:mx-auto after:bg-white  "></ul>
+      <div className="flex items-center justify-center px-6 py-3 lg:py-0 lg:px-0 lg:w-10 border-cyan right-0 fixed z-30 bottom-0 lg:right-3 xl:right-10 left-auto">
+        <div className="list-none after:hidden lg:after:block after:h-24 after:w-[2px] after:mx-auto after:bg-white after:mt-20 ">
+          <div className="md:rotate-90">
+            <Link href={`/`}>
+              <a className="text-text hover:text-cyan duration-200 font-pop text-sm font-semibold ">
+                xeuxdev@gmail.com
+              </a>
+            </Link>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
