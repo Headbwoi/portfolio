ee5b352d7af1fca0dff411e0f61ae3ec747f6ca9
headbwoi <tochukwujohn24@gmail.com>
2022-10-29 05:24:00 +0100

    white line added to contact icons

--- components/Utils/ContactIcons.tsx
+++ components/Utils/ContactIcons.tsx
@@ -1,6 +1,16 @@
-
 const ContactIcons = () => {
-  return <div>ContactIcons</div>
+  return (
+    <>
+      {/* ;eft */}
+      <div className="flex flex-col items-center justify-center w-10 border-cyan fixed z-30 bottom-0 left-10 right-auto">
+        <ul className="list-none after:block after:h-24 after:w-[2px] after:mx-auto after:bg-white  "></ul>
+      </div>
+      {/* right */}
+      <div className="flex flex-col items-center justify-center w-10 border-cyan fixed z-30 bottom-0 right-10 left-auto">
+        <ul className="list-none after:block after:h-24 after:w-[2px] after:mx-auto after:bg-white  "></ul>
+      </div>
+    </>
+  )
 }
 
 export default ContactIcons
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
-import { Header } from "../components"
+import { ContactIcons, Header } from "../components"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <ContactIcons />
     </>
   )
 }
