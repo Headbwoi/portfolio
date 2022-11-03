a77ee6c1d1cd6769185d18b022e1366af9932a1d
headbwoi <tochukwujohn24@gmail.com>
2022-11-03 22:51:46 +0100

    animate contact items

---
+++ variants/contactVariants.ts
@@ -0,0 +1,24 @@
+export const contactItemVariant = {
+  heading: {
+    init: {
+      opacity: 0,
+      y: 20,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.3, delay: 0.5 },
+    },
+  },
+  p1: {
+    init: {
+      opacity: 0,
+      y: 20,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.3, delay: 0.8 },
+    },
+  },
+}
