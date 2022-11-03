a0161c21350d6931994ccb80d1dd907dae5cd1d5
headbwoi <tochukwujohn24@gmail.com>
2022-11-03 22:36:29 +0100

    add contactform markup and styles

--- components/Main/ContactSection/ContactForm.tsx
+++ components/Main/ContactSection/ContactForm.tsx
@@ -1,7 +1,40 @@
 const ContactForm = () => {
   return (
-    <form>
-      <div></div>
+    <form className="flex flex-col justify-center items-center space-y-6 w-full">
+      <div className="h-14 w-full md:max-w-md lg:max-w-lg xl:max-w-xl">
+        <input
+          type="text"
+          name="name"
+          placeholder="what's your name?"
+          className="h-full w-full outline-none focus-visible:ring-[3px] focus-visible:ring-cyan focus:cyan hover:ring-cyan hover:ring-2 rounded-sm bg-gray text-white placeholder:text-text caret-cyan px-4 lg:px-6 text-base lg:text-lg"
+          aria-label="enter your name"
+        />
+      </div>
+      <div className="h-14 w-full md:max-w-md lg:max-w-lg xl:max-w-xl">
+        <input
+          type="email"
+          name="email"
+          placeholder="What's your @email address?"
+          className="h-full w-full outline-none focus-visible:ring-[3px] focus-visible:ring-cyan focus:cyan hover:ring-cyan hover:ring-2 rounded-sm bg-gray text-white placeholder:text-text caret-cyan px-4 lg:px-6 text-base lg:text-lg"
+        />
+      </div>
+      <div className="h-14 w-full md:max-w-md lg:max-w-lg xl:max-w-xl">
+        <input
+          type="text"
+          name="textarea"
+          placeholder="what do you have for me / offer "
+          className="h-full w-full outline-none focus-visible:ring-[3px] focus-visible:ring-cyan focus:cyan hover:ring-cyan hover:ring-2 rounded-sm bg-gray text-white placeholder:text-text caret-cyan px-4 lg:px-6 text-base lg:text-lg"
+        />
+      </div>
+      <div className="w-full h-14 md:max-w-md lg:max-w-lg xl:max-w-xl">
+        <button
+          type="submit"
+          aria-label="send me the email"
+          className="w-full h-full outline-none rounded-md bg-cyan_dark hover:bg-cyan duration-300 focus-visible:ring-cyan/70 focus-visible:ring-4 focus-visible:bg-cyan"
+        >
+          Send it to me
+        </button>
+      </div>
     </form>
   )
 }
--- pages/contactme.tsx
+++ pages/contactme.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next"
-import { ContactItems, HeadSection, PageTitle } from "../components"
+import { ContactForm, ContactItems, HeadSection } from "../components"
 
 const ContactMe: NextPage = () => {
   return (
@@ -7,12 +7,12 @@ const ContactMe: NextPage = () => {
       <HeadSection page="Contact" title="Portfolio - Contact Page" />
       <main className="relative font-pop  min-h-screen w-full bg-body overflow-x-hidden py-16 ">
         <div className="max-w-4xl xl:max-w-[1000px] px-6 md:px-10 lg:px-0 mx-auto">
-          <div className="flex items-center justify-center">
+          <div className="flex items-center justify-center flex-col">
             <section className="pt-11 lg:pt-24 pb-14">
               <ContactItems />
             </section>
-            <section className="pt-7 md:pt-10 lg:pt-14 flex justify-center flex-col">
-              <Conta
+            <section className="pt-7 md:pt-10 lg:pt-14 flex justify-center flex-col w-full">
+              <ContactForm />
             </section>
           </div>
         </div>
