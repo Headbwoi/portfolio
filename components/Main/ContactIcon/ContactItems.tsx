36d816e463b4079b062cc47d01fa2275849f55ff
headbwoi <tochukwujohn24@gmail.com>
2022-11-03 21:21:54 +0100

    contact items

---
+++ components/Main/ContactIcon/ContactItems.tsx
@@ -0,0 +1,11 @@
+const ContactIcon = () => {
+  return (
+    <div className="">
+      <div className="text-center text-cyan text-4xl md:text-6xl">
+        Get In Touch
+      </div>
+    </div>
+  )
+}
+
+export default ContactIcon
--- pages/contactme.tsx
+++ pages/contactme.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next"
-import { HeadSection, PageTitle } from "../components"
+import { ContactItems, HeadSection, PageTitle } from "../components"
 
 const ContactMe: NextPage = () => {
   return (
@@ -8,6 +8,9 @@ const ContactMe: NextPage = () => {
       <main className="relative font-pop  min-h-screen flex items-center justify-center  w-full bg-body overflow-x-hidden py-16 ">
         <div className="max-w-4xl xl:max-w-[1000px] px-6 md:px-10 lg:px-0">
           <PageTitle page="Contact" addon="Me" />
+          <section className="pt-10 lg:pt-20 pb-14">
+            <ContactItems />
+          </section>
         </div>
       </main>
     </>
