c6c9e508adf2c24669679464597fd2e6daab5edd
headbwoi <tochukwujohn24@gmail.com>
2022-11-03 21:51:08 +0100

    done with contactItems

--- components/Main/ContactIcon/ContactItems.tsx
+++ components/Main/ContactIcon/ContactItems.tsx
@@ -1,13 +1,14 @@
 const ContactIcon = () => {
   return (
     <div className="max-w-xl mx-auto">
-      <h1 className="text-center text-cyan text-4xl md:text-6xl font-semibold mb-4">
+      <h1 className="text-center text-cyan text-4xl md:text-6xl font-semibold mb-5">
         Get In Touch
       </h1>
-      <p className="text-text text-xl md:text-2xl text-center">
+      <p className="text-text text-lg md:text-xl text-center">
         Do you have an Idea about anything? or want to bring your idea or
-        presence to the web?. Have a chat with me via any of the links below or
-        send me an email using the form below
+        presence to the web?. Have a chat with me about your idea via any of the
+        links below or send me an email by filling the form below. My inbox is
+        always open.
       </p>
     </div>
   )
