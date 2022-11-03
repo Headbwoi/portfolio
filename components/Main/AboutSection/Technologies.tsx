ae2936fcb588a5d4bb96e22436b018c80b96a931
headbwoi <tochukwujohn24@gmail.com>
2022-11-03 11:54:19 +0100

    reduce width of tech cards

--- components/Main/AboutSection/Technologies.tsx
+++ components/Main/AboutSection/Technologies.tsx
@@ -36,11 +36,11 @@ export default Technologies
 
 function TechCard({ name }: TechCardProps) {
   return (
-    <div className="h-24 md:h-32 lg:h-40 rounded-md bg-buttonBg grid place-items-center cursor-pointer hover:scale-105 duration-200">
-      <div className="w-7 h-7 md:h-9 md:w-9 lg:w-12 lg:h-12 relative">
+    <div className="h-24 md:h-32 lg:h-36 rounded-md bg-buttonBg grid place-items-center cursor-pointer hover:scale-105 duration-200">
+      <div className="w-7 h-7 md:h-9 md:w-9 lg:w-10 lg:h-10 relative">
         <Image src={`/images/${name}.svg`} alt={`${name}`} layout="fill" />
       </div>
-      <div className="text-white text-base md:text-xl lg:text-2xl xl:text-3xl font-normal tracking-wide capitalize">
+      <div className="text-white text-base md:text-xl lg:text-2xl font-normal tracking-wide capitalize">
         {name}
       </div>
     </div>
