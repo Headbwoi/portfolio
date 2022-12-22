ecf084bac0e08d7e24f84eb5a3f0629837ceb45e
headbwoi <tochukwujohn24@gmail.com>
2022-12-22 03:33:56 +0100

    made resume local

--- components/Main/HeroSection/ResumeButton.tsx
+++ components/Main/HeroSection/ResumeButton.tsx
@@ -6,10 +6,7 @@ const ResumeButton = () => {
     <button
       className={`flex items-center justify-center rounded-md px-5 w-fit h-11 bg-buttonBg  hover:bg-cyan_dark duration-300 group cursor-pointer`}
     >
-      <Link
-        href={`https://drive.google.com/file/d/1q3uB910U8KOgeOoVpWG5UbPNHBHdSRzH/view`}
-        target="_blank"
-      >
+      <Link href={`/docs/resume.pdf`} target="_blank">
         <div className="flex w-full h-full items-center">
           <span className="text-white text-sm mr-3 group:hover:mr-1 font-normal capitalize tracking-wide">
             view resume
